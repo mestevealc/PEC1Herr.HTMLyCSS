227985508eec500490ab7e695a7502fe6e7769ad
Maria Esteve Alcala <mestevealc@uoc.edu>
2023-11-23 17:45:56 +0100

    javascript y retoques estetica

--- src/app.js
+++ src/app.js
@@ -1,2 +1,34 @@
-const name = "world";
-console.log(`Hello ${name}`);
+document.addEventListener("DOMContentLoaded", function () {
+  let botonPresentacion = document.querySelector(".verMas");
+
+  botonPresentacion.addEventListener("click", function () {
+    let nombres = [
+      "Winona Ryder",
+      "David Harbour",
+      "Finn Wolfhard",
+      "Millie Bobby Brown",
+      "Gaten Matarazzo",
+      "Caleb McLaughlin",
+      "Natalia Dyer",
+      "Charlie Heaton",
+      "Cara Buono",
+      "Matthew Modine",
+      "Noah Schnapp",
+      "Sadie Sink",
+      "Joe Keery",
+      "Dacre Montgomery",
+      "Sean Astin",
+      "Paul Reiser",
+      "Maya Hawke",
+      "Priah Ferguson",
+      "Brett Gelman",
+      "Jamie Campbell Bower",
+      "Eduardo Franco",
+      "Joseph Quinn",
+    ];
+
+    let listaNombres = nombres.join("\n");
+
+    alert(listaNombres);
+  });
+});
